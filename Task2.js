db5c129eadb8b249788875dcd284c29a0541642e
andriabakti <andr.bkti@gmail.com>
2020-07-19 16:24:42 +0700

    Tugas1.2

---
+++ Task2.js
@@ -0,0 +1,24 @@
+const mtk = null
+const bahasaIndonesia = 86
+const bahasaInggris = 94
+const ipa = 77
+
+let jumlah = mtk + bahasaIndonesia + bahasaInggris + ipa
+if(mtk==null || bahasaIndonesia==null || bahasaInggris==null || ipa==null) {
+    console.log("Semua nilai harus diisi !")
+}
+
+let rata = jumlah/4
+console.log("Rata-rata = " + rata);
+
+if(rata<=59) {
+    console.log("Grade = E")
+} else if (rata<=69) {
+    console.log("Grade = D")
+} else if (rata<=79) {
+    console.log("Grade = C")
+} else if (rata<=89) {
+    console.log("Grade = B")
+} else if (rata<=100) {
+    console.log("Grade = A")
+}
---
+++ Task4.js
@@ -0,0 +1,31 @@
+let data = {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address:
+    {
+        street: "Kulas Light",
+        suite: "Apt. 556",
+        city: "Gwenborough",
+        zipcode: "92998-3874",
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+}
+
+let dataBaru = {
+    name: "Andria Bakti Mahendra",
+    email: "andr.bkti@gmail.com",
+    hobby: [
+        "Nonton Anime",
+        "Membaca"
+    ]
+}
+
+data2 = {...data, ...dataBaru};
+console.log(data2)
+
+const {street, city} = data.address
+console.log(street)
+console.log(city)
